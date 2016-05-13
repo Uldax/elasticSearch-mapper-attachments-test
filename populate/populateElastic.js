1420e1fd76dca8fa4575a63628a40c5891f77d35
pcontat <contat.paul@gmail.com>
2016-05-13 10:47:51 -0400

    add document right in DB

--- populate/populateElastic.js
+++ populate/populateElastic.js
@@ -5,21 +5,27 @@ var elasticSearchPort = "9200";
 var protocol = "http"
 var indexName = "test/person"
 var serverIp = "localhost";	
-
+var baseURL = protocol+"://"+serverIp+":"+elasticSearchPort+"/"+indexName;
 //Folder parameters
 var folderName = "indexedDocuments";
-var baseURL = protocol+"://"+serverIp+":"+elasticSearchPort+"/"+indexName;
-
-//Todo get the number of total files already indexed first
-var nbr=10;
 
 //Option for resquest
 var options = {
     method: 'POST',
+    url :  baseURL+"/",
     headers : {
-        'Content-Type': 'application/x-www-form-urlencoded'
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Transfer-Encoding':'chunked'
     }
 };
+
+
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL || 'postgres://superopus:superopus@localhost:5432/documentBase';
+var client = new pg.Client(connectionString);
+client.connect();
+
+
 
 
 // function to encode file data to base64 encoded string
@@ -29,14 +35,24 @@ function base64_encode(file) {
     // convert binary data to base64 encoded string
     return new Buffer(bitmap).toString('base64');
 }
+
+function insertDocumentIntoDB(filename) {
+    //Add the pdf in database
+    client.query("INSERT INTO document(document_name,application_id) values($1, $2) RETURNING document_id;", [filename, 1], function (err, result) {
+        console.log(result);
+    });
+    //var readPrivilege = randomIntInc(1,3);
+    //client.query("INSERT INTO pdf_privilege(privilege_id, pdf_id) values($1, $2)", [data.text, readPrivilege]);                                 
+    query.on('end', function () { client.end(); });
+}
     
 // function to index file in elastic serveur by REST API
-function indexFile(filename,index) { 
+function indexFile(filename) { 
     
     isIndexed(filename).then(function(){
         var base64file = base64_encode("../"+folderName+"/"+filename);
-        console.log("File "+filename+" loaded") ; 
-
+        var fileSize = Buffer.byteLength(base64file);
+        
         //object depending on elastic mapping    
         //TODO add more metadata             
         var requestData = {   
@@ -45,25 +61,29 @@ function indexFile(filename,index) {
                 "_name" : filename,
             }
         }  
+        //Add object and content lenght header
         options.json = requestData;
-        options.url =  baseURL+"/"+ index;
+        options.headers["Content-Length"] = fileSize;
 
-        console.log("Request send to " + options.url) ;   
         //index file
         request(options, function(err, response, body) {
             if (!err) {
-                if(response.statusCode === 200) {    
+                if(response.statusCode === 201 || response.statusCode === 200 ) {    
                     if (typeof body != undefined) {
-                        console.log("Creation of "+filename+" state : " + body.created);
+                        
+                        console.log("Creation of "+filename+" : " + body.created);
                     } 
                 } else {
                     if (response.statusCode === 400) {
                         console.log('Bad Request');
                     }
-                    console.log(response.statusCode);
+                    console.log( 'Unhandled status code : '+ response.statusCode);
                 }         
             } else{
-                console.log(err);
+                if(err.code == "ECONNRESET" && fileSize > 104857600 ){
+                    console.log("Connexion reset : "+filename+" content length exceeded 104857600 bytes " + "("+fileSize+")");
+                }
+                else console.log(err);
             }   
         });
     })   
@@ -98,7 +118,7 @@ function isIndexed(fileName){
                     if (body.hits.total == 0) {
                         resolve(body);
                     } else {                
-                        reject("File "+fileName+" already indexed , change the name");
+                        reject("File with name "+fileName+" is already indexed");
                     }
                 } else {
                     if (response.statusCode === 400) {
@@ -117,14 +137,15 @@ function readFolder(dirname,onError) {
       onError(err);
       return;
     }
-    filenames.forEach(function(filename,index) {
-      indexFile(filename,index)
+    filenames.forEach(function(filename) {
+      indexFile(filename)
     });
   });
 }
  
 
- 
+
+//Main
 readFolder("../"+folderName, function(err){
     console.log("Error occured")
     console.log(err);
