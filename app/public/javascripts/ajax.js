f78e7b92e483c55eead919ffeb2ba1ff89ed8366
Cedric <Cedric>
2016-05-16 11:43:11 -0400

    AJAX request to server

---
+++ app/public/javascripts/ajax.js
@@ -0,0 +1,48 @@
+//get buttonSearch, search string, auth level
+var buttonSearch = document.querySelector("#submitButton");
+var inputSearch = document.querySelector("#inputSearch");
+var userPrivilege = document.querySelector("#userPrivilege");
+
+//set eventlidtener onclick call function 
+buttonSearch.addEventListener("click", ajaxCall);
+
+
+function ajaxCall(evt) {
+    evt.preventDefault();
+    var xhr = null;
+    
+    if (window.XMLHttpRequest || window.ActiveXObject) {
+        if (window.ActiveXObject) {
+            try {
+                xhr = new ActiveXObject("Msxml2.XMLHTTP");
+            } catch (e) {
+                xhr = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+        } else {
+            xhr = new XMLHttpRequest();
+        }
+    } else {
+        console.log("Votre navigateur ne supporte pas l'objet XMLHTTPRequest...");
+        return;
+    }
+    
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
+			readData(xhr.responseText);
+		}
+    };
+     
+    //akax vers /search?search=XX&authLevel=XX
+    xhr.open("GET", "/search?search=" + inputSearch.value + "&authAccess=" + userPrivilege.value, true);
+    xhr.send(null);
+    
+}
+
+    
+
+    function readData(data) {
+        myObject = JSON.parse(data);
+        for (node in myObject.hits.hits){
+            console.log(myObject.hits.hits[node]._source.my_attachment._name);
+        }
+    }
