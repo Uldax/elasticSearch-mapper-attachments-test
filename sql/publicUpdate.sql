03ed6e7f13a632d778067e7c11c4b9bf122f15fc
pcontat <contat.paul@gmail.com>
2016-05-24 09:41:51 -0400

    add public update

---
+++ sql/publicUpdate.sql
@@ -0,0 +1,7 @@
+﻿DROP TABLE IF EXISTS public.update;
+CREATE TABLE public.update(
+	update_id    SERIAL NOT NULL ,
+	op CHAR NOT NULL,
+	updated TIMESTAMP NOT NULL,
+	CONSTRAINT prk_constraint_update PRIMARY KEY (update_id)
+)WITHOUT OIDS;
