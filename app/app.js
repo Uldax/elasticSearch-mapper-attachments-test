c884e4202215972b2ec4dea4c816ea62cbeaafbe
contat <contat.paul@gmail.com>
2016-07-03 11:08:01 -0400

    ip filter for postman and localhost only

--- app/app.js
+++ app/app.js
@@ -1,17 +1,32 @@
 var express = require('express');
 var path = require('path');
+
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var ipfilter = require('express-ipfilter');
 var routes = require('./controllers/index');
 var elasticUpdater = require('./elasticSearch/updater/elasticUpdater');
 var elasticImporter = require('./elasticSearch/elasticImporter');
 var testM = require('./models/test');
+var util = require('util');
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+// a first middleware, a logger in console, just to show ip
+app.use(function(req, res, next){
+  console.log('%s %s from %s , proxy: %s', req.method, req.url, req.ip, util.inspect(req.ips));
+  next();
+});
+
+//Whitelisting certain IP addresses, while denying all other IPs:
+//Postman and local host
+var ips = ['::ffff:127.0.0.1','127.0.0.1'];
+app.use(ipfilter(ips, {mode: 'allow'}));
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,16 +37,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 app.use('/', routes);
 
-//Set up elastic updater
-//Warning may be heavy
-if (process.argv[2] === "import") {
-    console.log("Import Process ..");
-    elasticImporter.start();
-}
-else{
-    elasticUpdater.start();
-    //testM.crawlFoler("../indexedDocuments");
-}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -63,6 +68,19 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+
+
+//Set up elastic updater
+//Warning may be heavy
+if (process.argv[2] === "import") {
+    console.log("Import Process ..");
+    elasticImporter.start();
+}
+else{
+    elasticUpdater.start();
+    //testM.crawlFoler("../indexedDocuments");
+}
+
 
 
 module.exports = app;
